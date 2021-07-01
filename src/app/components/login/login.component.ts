07d72127d5196092add988942770cb1572b17484
Ajit Sant <ajitkumarsantx@gmail.com>
2021-07-01 17:17:02 +0530

    custom scrollbar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import { EncrDecrService } from 'src/app/services/encr-decr.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,10 @@ import { ApiService } from 'src/app/services/api.service';
 export class LoginComponent {
   loginForm: FormGroup;
   errMsg = '';
-  constructor(private fb: FormBuilder, private router: Router, private apiSrv: ApiService) {
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private apiSrv: ApiService,
+    private EncrDecr: EncrDecrService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       password: ['', Validators.required],
@@ -22,7 +26,9 @@ export class LoginComponent {
     e.preventDefault();
     this.loginForm.markAllAsTouched();
     if (this.loginForm.valid) {
-      this.apiSrv.login(this.loginForm.value).subscribe({
+      const email = this.loginForm.value.email;
+      const password = this.EncrDecr.set('123456$#@$^@1ERF', this.loginForm.value.password);
+      this.apiSrv.login({email,password}).subscribe({
         next: (res: any) => {
           if (res.accessToken) {
             localStorage.setItem('token', res.accessToken);
